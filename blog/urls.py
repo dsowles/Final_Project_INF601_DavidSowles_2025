ede15d97014b3f04ed376bda12f9340323f47010
dsowles <d_sowles@mail.fhsu.edu>
2025-12-05 12:00:00 -0600

    major: accounts app created.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.index)
+    path('blog/', views.index,name='home'),
+    path('blog/<slug:slug>/', views.detail, name='blog-detail'),
+    path('blog/create', views.create, name='blog-create'),
+    path('blog/delete', views.delete, name='blog-delete'),
+    path('blog/update', views.update, name='blog-update'),
+    path('blog/search', views.search, name='blog-search')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
-#from django.http import HttpResponse
+
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.filter(published=True).order_by('-created_at')
+    return render(request, 'index.html', {'posts': posts})
+
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, 'detail.html', {'post': post})
+
+def create(request):
+    return HttpResponse('Create Blog Page')
+
+def delete(request):
+    return HttpResponse('Delete Blog Page')
+
+def update(request):
+    return HttpResponse('Update Blog Page')
+
+def search(request):
+    return HttpResponse('Search Blog Page')
