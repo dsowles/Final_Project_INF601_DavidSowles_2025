3e2d52ee40df4e3bf5232c49d750bd9e8efff816
dsowles <d_sowles@mail.fhsu.edu>
2025-12-05 12:00:00 -0600

    Changed url patterns

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.index,name='home'),
-    path('blog/<slug:slug>/', views.detail, name='blog-detail'),
-    path('blog/create', views.create, name='blog-create'),
-    path('blog/delete/<slug:slug>/', views.delete, name='blog-delete'),
-    path('blog/update/<slug:slug>/', views.update, name='blog-update'),
-    path('blog/search/', views.search, name='blog-search'),
+    path('', views.index,name='home'),
+    path('create', views.create, name='blog-create'),
+    path('delete/<slug:slug>/', views.delete, name='blog-delete'),
+    path('update/<slug:slug>/', views.update, name='blog-update'),
+    path('search/', views.search, name='blog-search'),
     path('account/', views.account_dashboard, name='account-dashboard'),
+    path('<slug:slug>/', views.detail, name='blog-detail'),
 ]
--- the_fiction_archive/urls.py
+++ the_fiction_archive/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from blog import views
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', include('blog.urls'))
+    path('', views.index, name='home'),
+    path('blog/', include('blog.urls')),
 ]
